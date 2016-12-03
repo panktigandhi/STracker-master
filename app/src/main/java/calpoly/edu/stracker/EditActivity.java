7a46f7c9ab702ca42874d72d07f3b18b1e8578df
panktigandhi <pankti_94@hotmail.com>
2016-12-03 00:04:06 -0800

    Edit Activity and menu_add

---
+++ app/src/main/java/calpoly/edu/stracker/EditActivity.java
@@ -0,0 +1,94 @@
+package calpoly.edu.stracker;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class EditActivity extends AppCompatActivity {
+    String itemTitle, date;
+    String amount;
+    EditText editDescription, editDate, editAmount;
+    DatabaseHelper mydb;
+    Calendar transactionCalendar;
+    private DatePickerDialog fromDatePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            itemTitle = extras.getString("ItemTitle");
+            date = extras.getString("date");
+            amount = extras.getString("amount");
+        }
+        editDescription = (EditText) findViewById(R.id.editDesc);
+        editDate = (EditText) findViewById(R.id.editDate);
+        editAmount = (EditText) findViewById(R.id.editAmount);
+        editDescription.setText(itemTitle);
+        editDate.setText(date);
+        editAmount.setText(amount);
+        mydb = new DatabaseHelper(this);
+        getSupportActionBar().setTitle("Edit Transaction");
+        editDate.setInputType(InputType.TYPE_NULL);
+        editDate.requestFocus();
+        editDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                fromDatePickerDialog.show();
+            }
+        });
+        transactionCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(this, R.style.MyDatePickerTheme, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                transactionCalendar.set(year, monthOfYear, dayOfMonth);
+                editDate.setText(DatabaseHelper.convertHumanDate(transactionCalendar));
+            }
+        }, transactionCalendar.get(Calendar.YEAR),
+                transactionCalendar.get(Calendar.MONTH),
+                transactionCalendar.get(Calendar.DAY_OF_MONTH));
+
+        editDate.setText(DatabaseHelper.convertHumanDate(transactionCalendar));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_done, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.menu_done) {
+            updateTransaction();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void updateTransaction() {
+        boolean isUpdate = mydb.updateData(editDescription.getText().toString(), editDate.getText().toString(), editAmount.getText().toString(), "fuel");
+        Log.d("editDescription", editDescription.getText().toString());
+        Log.d("editDate", editDate.getText().toString());
+        Log.d("editAmount", editAmount.getText().toString());
+
+        if (isUpdate == true) {
+            Toast.makeText(getApplicationContext(), "Update Success", Toast.LENGTH_LONG).show();
+        } else
+            Toast.makeText(getApplicationContext(), "Update Fail", Toast.LENGTH_LONG).show();
+        finish();
+    }
+}
