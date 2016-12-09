ffbc0904c18cc6ffe7c7b5842ba351844dd3d1cf
makkabeus <mak.proxy@gmail.com>
2016-12-08 16:59:39 -0800

    Refactored activity_add to have decimals
    Removed unused imports from MainActivity

--- app/src/main/java/calpoly/edu/stracker/MainActivity.java
+++ app/src/main/java/calpoly/edu/stracker/MainActivity.java
@@ -2,22 +2,13 @@ package calpoly.edu.stracker;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,14 +20,13 @@ public class MainActivity extends AppCompatActivity {
     SlidingTabLayout tabs;
     CharSequence Titles[] = {"Summary", "Transactions", "Categories"};
     int Numboftabs = 3;
-    String shareData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TransactionManager.transactionManagerInit(getApplicationContext());
         CategoryManager.categoryManagerInit(getApplicationContext());
+        TransactionManager.transactionManagerInit(getApplicationContext());
         mydb = new DatabaseHelper(this);
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
